04c1d4a54f4ef22f841b8e85383d82f96b301438
Vineet Singh <54494603+vineetsingh-vs@users.noreply.github.com>
2025-03-02 13:10:56 -0800

    feat(OAuth)MAICRG-4-] dockerization

--- models/index.js
+++ models/index.js
@@ -11,8 +11,12 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
+  console.log("Menv", env);
+  console.log("host", config.host);
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
+  console.log("Menv", env);
+  console.log("host", config.host);
   sequelize = new Sequelize(config.database, config.username, config.password, {
     host: config.host,
     dialect: config.dialect,
